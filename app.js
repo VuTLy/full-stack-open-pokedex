998c3d74e42f3514e32da5f4b655c2cdb0da7e4b
VuTLy <vulyccschool@gmail.com>
2025-08-26 13:42:08 -0700

    should fail

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 5000
 // Serve static files from 'dist'
 app.use(express.static('dist'))
 
-// Health check endpoint
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
