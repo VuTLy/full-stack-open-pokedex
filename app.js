90c54e894f19d19058915aa4246aeace23ca104a
VuTLy <vulyccschool@gmail.com>
2025-08-26 13:39:52 -0700

    add health check point

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
+// Serve static files from 'dist'
 app.use(express.static('dist'))
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
